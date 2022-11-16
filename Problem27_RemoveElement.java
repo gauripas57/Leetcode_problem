e6900a6990d3e6784d16054e7ac36f9a2fcad7ca
pasarkarg <pasarkar.g@northeastern.edu>
2022-11-16 12:24:30 -0500

    Day2

---
+++ Problem27_RemoveElement.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int writer = 0;
+        int n = nums.length;
+        
+        for(int reader = 0; reader < n ; reader ++){
+            if(nums[reader]!= val){
+                nums[writer++] =nums[reader];
+            }
+        }
+        return writer;
+        
+    }
+}
